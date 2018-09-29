bd80e48695a2053038e93aa797f44725fc3d9f3d
Wong <thatlostboy@gmail.com>
2018-09-29 00:33:38 -0700

    added submit form

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -15,11 +15,24 @@ $(function () {
         );
     })
 
-    $("#addburger").on("submit", function (event) {
+    $("#addburger").on("click", function (event) {
         event.preventDefault();
         newburger = $("#burgername").val().trim();
 
-        console.log(newburger)
+        console.log("added new burger: ", newburger);
+
+        // Send the POST request
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: { 'burger_name':newburger }
+        }).then(
+            function () {
+                console.log("added new burger: ", newburger);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+        
     })
 
 });
